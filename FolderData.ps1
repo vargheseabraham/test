5e9a31054bb20df1b96c96ac6316777c6d30480e
git_admin <git_admin@gmail.com>
2018-11-21 15:38:01 +0530

    Second commit

---
+++ FolderData.ps1
@@ -0,0 +1,9 @@
+﻿$fullPath = (Get-ChildItem -Path "C:\DataCollection\" -Force -Recurse).FullName
+$fullName = $fullPath | ?{$_ -like '*ApplicationUsage*'}
+
+$allApps = @()
+foreach($fName in $fullName)
+{
+    $csvData = Import-Csv $fName
+    $allApps += ($csvData).CurrentApplication
+}
